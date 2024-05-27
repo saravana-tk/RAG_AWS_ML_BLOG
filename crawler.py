9b231206f1c22e2bad53ca75dbaba95f9c4befcd
Saravana T Kathirvel <saravana_tk@Saravanas-MacBook-Pro.local>
2024-05-26 21:02:51 -0400

    Removed unnecessary imports

--- crawler.py
+++ crawler.py
@@ -5,9 +5,6 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_astradb import AstraDBVectorStore
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_core.documents import Document
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import OpenAIEmbeddings
 
 
